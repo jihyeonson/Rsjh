2237b10137f59da7eed81622fb20164ab38b4b45
jihyeonson <45547366+jihyeonson@users.noreply.github.com>
2018-12-04 11:41:05 +0900

    Add files via upload

--- wel.R
+++ wel.R
@@ -1,5 +1,6 @@
 # 09. 데이터 분석 프로젝트
 # 09-1. '한국복지패널데이터' 분석 준비하기
+
 install.packages("foreign") # foreign 패키지 설치 
 library(foreign)# SPSS 파일 로드 
 install.packages("dplyr")
@@ -31,6 +32,8 @@ welfare <- rename(welfare, sex = h10_g3, # 성별
 
 str(welfare)
 View(welfare$sex)
+
+
 # 09-2. 성별에 따른 월급 차이
 - "성별에 따라 월급이 다를까?"
 
@@ -79,6 +82,7 @@ ggplot(data = sex_income, aes(x = sex, y = mean_income)) + geom_col()
 
 # 09-3. 나이와 월급의 관계
 # - "몇 살 때 월급을 가장 많이 받을까?"   pdf 17쪽
+
 class(welfare$birth)
 summary(welfare$birth)
 qplot(welfare$birth)
@@ -107,41 +111,175 @@ head(age_income)
 ggplot(data = age_income, aes(x = age, y = mean_income)) + geom_line()
 
 
+# 1. 나이에 따른 월급 평균표 만들기 
+age_income <- welfare %>% filter(!is.na(income)) %>% group_by(age) %>% summarise(mean_income = mean(income)) 
+head(age_income)
+
+ggplot(data = age_income, aes(x = age, y = mean_income)) + geom_line()
 
 
+# 연령대 변수 검토 및 전처리하기
+# 파생변수 만들기 - 연령대 
+welfare <- welfare %>% mutate(ageg = ifelse(age < 30, "young", ifelse(age <= 59, "middle", "old"))) 
+table(welfare$ageg)
+
+qplot(welfare$ageg)
+
+# 연령대에 따른 월급 차이 분석하기
+# 1. 연령대별 월급 평균표 만들기 
+ageg_income <- welfare %>% filter(!is.na(income)) %>% group_by(ageg) %>% summarise(mean_income = mean(income)) 
+ageg_income
+
+ggplot(data = ageg_income, aes(x = ageg, y = mean_income)) + geom_col()
+
+# 막대 정렬 : 초년, 중년, 노년 나이 순 
+ggplot(data = ageg_income, aes(x = ageg, y = mean_income)) + geom_col() + scale_x_discrete(limits = c("young", "middle", "old"))
 
 
+# 9-5
+# 연령대 및 성별 월급 차이 분석하기
+# 1. 연령대 및 성별 월급 평균표 만들기 
+sex_income <- welfare %>% 
+  filter(!is.na(income)) %>% 
+  group_by(ageg, sex) %>% 
+  summarise(mean_income = mean(income)) 
+sex_income
 
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) + 
+  geom_col() + scale_x_discrete(limits = c("young", "middle", "old"))
 
+# 성별 막대 분리 
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) + geom_col(position = "dodge") + scale_x_discrete(limits = c("young", "middle", "old"))
 
+# 성별 연령별 월급 평균표 만들기
+sex_age <- welfare %>%
+  filter(!is.na(income)) %>%
+  group_by(age, sex) %>%
+  summarise(mean_income = mean(income))
+head(sex_age)
 
+#그래프 만들기
+ggplot(data = sex_age, aes(x = age, y = mean_income, col = sex)) + geom_line()
 
+#09-6. 직업별 월급 차이
+- "어떤 직업이 월급을 가장 많이 받을까?"
+#변수 검토하기
+class(welfare$code_job)
+table(welfare$code_job)
 
+#전처리
+#직업분류코드 목록 불러오기
+library(readxl)
+list_job <- read_excel("Koweps_Codebook.xlsx", col_names = T, sheet = 2)
+head(list_job)
 
+#welfare에 직업명 결합
+welfare <- left_join(welfare, list_job, id = "code_job")
+welfare %>%
+  filter(!is.na(code_job)) %>%
+  select(code_job, job) %>%
+  head(10)
 
+#직업별 월급 차이 분석하기
+#1. 직업별 월급 평균표 만들기
 
+job_income <- welfare %>%
+  filter(!is.na(job) & !is.na(income)) %>%
+  group_by(job) %>%
+  summarise(mean_income = mean(income))
+head(job_income)
 
+#상위 10개 추출
+top10 <- job_income %>%
+  arrange(desc(mean_income)) %>%
+  head(10)
+top10
+#그래프 만들기 
+ggplot(data = top10, aes(x = reorder(job, mean_income), y = mean_income)) + 
+  geom_col() + 
+  coord_flip()
+#하위 10위 추출 
+bottom10 <- job_income %>% 
+  arrange(mean_income) %>% 
+  head(10) 
+bottom10
 
+#그래프 만들기 
+ggplot(data = bottom10, aes(x = reorder(job, -mean_income), y = mean_income)) + 
+  geom_col() + 
+  coord_flip() + 
+  ylim(0, 850)
+#09-7. 성별 직업 빈도
+- "성별로 어떤 직업이 가장 많을까?"
+#성별 직업 빈도표 만들기 
+# 남성 직업 빈도 상위 10개 추출 
+job_male <- welfare %>% 
+  filter(!is.na(job) & sex == "male") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10) 
+job_male
 
+# 여성 직업 빈도 상위 10개 추출 
+job_female <- welfare %>% 
+  filter(!is.na(job) & sex == "female") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10) 
+job_female
 
+#그래프 만들기 
+# 남성 직업 빈도 상위 10개 직업 
+ggplot(data = job_male, aes(x = reorder(job, n), y = n)) + 
+  geom_col() + 
+  coord_flip()
+# 여성 직업 빈도 상위 10개 직업 
+ggplot(data = job_female, aes(x = reorder(job, n), y = n)) + 
+  geom_col() + 
+  coord_flip()
 
+#09.8. 종교 유무에 따른 이혼율, 종교가 있는 서람들이 이혼을 덜 할까?
+#변수 검토하기
+class(welfare$religion)
+table(welfare$religion)
 
+#전처리
+#종교 유무이름 부여
+welfare$religion <- ifelse(welfare$religion == 1, "yes", "no") 
+table(welfare$religion)
+qplot(welfare$religion)
 
+#혼인 상태 변수 검토 및 전처리
+#변수 검토하기
+class(welfare$marriage) 
+table(welfare$marriage)
 
+#전처리
+#이혼 여부 변수 만들기
+welfare$group_marriage <- ifelse(welfare$marriage == 1, "marriage", 
+                                 ifelse(welfare$marriage == 3, "divorce", NA)) 
+table(welfare$group_marriage)
+table(is.na(welfare$group_marriage))
+qplot(welfare$group_marriage)
 
+#종교 유무에 따른 이혼율 분석
+# 종교 유무에 따른 이혼율 표 만들기
+religion_marriage <- welfare %>% 
+  filter(!is.na(group_marriage)) %>% 
+  group_by(religion, group_marriage) %>% 
+  summarise(n = n()) %>% 
+  mutate(tot_group = sum(n)) %>% 
+  mutate(pct = round(n/tot_group*100, 1)) 
+religion_marriage
 
-
-
-
-
-
-
-
-
-
-
-
-
+#count() 활용
+religion_marriage <- welfare %>% 
+  filter(!is.na(group_marriage)) %>% 
+  count(religion, group_marriage) %>% 
+  group_by(religion) %>% 
+  mutate(pct = round(n/sum(n)*100, 1))
 
 
 
